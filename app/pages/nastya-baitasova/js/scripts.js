8b45ac4d1e5fd6bfb09a83f0669a86b73bb02ace
Anastasiia <baitasova.anastasiia@gmail.com>
2016-09-05 13:22:41 +0300

    fix some design issues

--- app/pages/nastya-baitasova/js/scripts.js
+++ app/pages/nastya-baitasova/js/scripts.js
@@ -1,3 +1,6 @@
 /**
  * Created by Admin on 28.08.2016.
  */
+
+var element = document.getElementById('hobby');
+element.innerHTML = ['танцы', ' спорт', ' вышивка крестом', ' психология', ' кулинария'];
