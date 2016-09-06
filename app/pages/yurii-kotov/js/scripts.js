0dccab5d27a8f6b97ab02efb22dd39514cf4c38b
Yurii Kotov <y_v_kotov@mail.ru>
2016-09-06 22:21:26 +0300

    ...

--- app/pages/yurii-kotov/js/scripts.js
+++ app/pages/yurii-kotov/js/scripts.js
@@ -3,13 +3,20 @@
  */
 window.onload = function anyDoing() {
     var bornYear = ['16 ноября 1988г']
-    var hobby = ['Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
+    var hobby = [' Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
     hobby.push(' Хз что я еще могу делать');
 
     var addYear = document.getElementById('user-info__date');
     var addHobby = document.getElementById('user-info__hobby');
-
+    var creatTag = document.createElement('li');
+    creatTag.className = 'user-info__hobby';
     addYear.innerHTML = bornYear;
-    addHobby.innerHTML = hobby;
+
+    function addToHtml(item) {
+        return addHobby.appendChild(creatTag);
+    }
+
+    hobby.forEach(addToHtml);
+
 
 }
