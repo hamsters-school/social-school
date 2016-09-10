3ba94e5f088c7f4206843600fe275214ead76bb9
Yurii Kotov <y_v_kotov@mail.ru>
2016-09-10 22:18:08 +0300

    some change

--- app/pages/yurii-kotov/js/scripts.js
+++ app/pages/yurii-kotov/js/scripts.js
@@ -2,7 +2,7 @@
  * Created by Admin on 28.08.2016.
  */
 window.onload = function anyDoing() {
-    var bornYear = ['16 ноября 1988г']
+    var bornYear = ['16 ноября 1988г'];
     var hobby = [' Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
     hobby.push(' Хз что я еще могу делать');
 
@@ -16,34 +16,53 @@ window.onload = function anyDoing() {
 
     hobby.forEach(function (item) {
         var creatTag = document.createElement('li');
-        creatTag.className = 'user-info__hobby'
-        addHobby.appendChild(creatTag).innerHTML = item + ', ';
+        creatTag.className = 'user-info__hobby';
+        if (item == hobby.slice(-1)) {
+            addHobby.appendChild(creatTag).innerHTML = item + '; ';
+        } else {
+            addHobby.appendChild(creatTag).innerHTML = item + ',';
+        }
+
     });
 
     function getNewsFromGoogle() {
+
         var requestNews = new XMLHttpRequest();
 
-        requestNews.open('get', "news.json", false);
-        requestNews.send();
+        requestNews.open('get', 'news.json', false);
+        requestNews.send;
 
-        requestNews.onreadystatechange = function () {
-            if (requestNews.readyState != 4) return;
-            if (requestNews.status != 200) {
-                console.log(requestNews.status + ': ' + requestNews.statusText);
-            } else {
-                var googleNews = JSON.parse(requestNews.responseText);
-                /*  showNews(googleNews);*/
+        //var newsReady = JSON.parse(requestNews.responseText);
+
+
+
+    }
+    getNewsFromGoogle();
+
+    /*    function getNewsFromGoogle() {
+            var requestNews = new XMLHttpRequest();
+
+            requestNews.open('get', "news.json", false);
+            requestNews.send();
+
+            requestNews.onreadystatechange = function () {
+                if (requestNews.readyState != 4) return;
+                if (requestNews.status != 200) {
+                    console.log(requestNews.status + ': ' + requestNews.statusText);
+                } else {
+                    var googleNews = JSON.parse(requestNews.responseText);
+                    showNews(googleNews);
+                }
+
+            }
+
+            function showNews(googleNews) {
+                googleNews.forEach(function (newsName) {
+                    var getNews = document.getElementById('news-get');
+                    var articleName = getNews.appendChild(document.createElement('li'));
+                })
             }
 
         }
-
-        function showNews(googleNews) {
-            googleNews.forEach(function (newsName) {
-                var getNews = document.getElementById('news-get');
-                var articleName = getNews.appendChild(document.createElement('li'));
-            })
-        }
-
-    }
-    getNewsFromGoogle() /*document.getElementById('newsGet').appendChild(document.createElement(article)).innerHTML = requestNews.responseText;*/
+        getNewsFromGoogle()*/
 }
