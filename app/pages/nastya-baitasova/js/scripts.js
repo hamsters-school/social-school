43da70454fefda06ec289f03c51070d0e164e91a
Anastasiia <baitasova.anastasiia@gmail.com>
2016-09-11 19:48:24 +0300

    create elements li in Js

--- app/pages/nastya-baitasova/js/scripts.js
+++ app/pages/nastya-baitasova/js/scripts.js
@@ -1,6 +1,17 @@
 /**
  * Created by Admin on 28.08.2016.
  */
+var hobbyArray = ['танцы', 'спорт', 'вышивка крестом', 'психология', 'кулинария'];
 
-var element = document.getElementById('hobby');
-element.innerHTML = ['танцы', ' спорт', ' вышивка крестом', ' психология', ' кулинария'];
+function hobbyList(hobbyArray) {
+    var element = document.getElementById('hobby');
+
+    for (var i = 0; i < hobbyArray.length; i++) {
+        var newLi = document.createElement('li');
+        newLi.innerHTML = hobbyArray[i];
+        element.appendChild(newLi);
+    }
+
+}
+
+hobbyList(hobbyArray);
