8a1d5856e268a54d4c4a47fc7103735ca3ea7db5
Anastasiia <baitasova.anastasiia@gmail.com>
2016-09-29 10:13:33 +0300

    create function li delete

--- app/pages/nastya-baitasova/js/workLocalStorage.js
+++ app/pages/nastya-baitasova/js/workLocalStorage.js
@@ -7,16 +7,32 @@ function storage() {
         var newLi = document.createElement("li");
         localStorage.setItem(text, text);
         newLi.innerHTML = text;
+        newLi.setAttribute("onclick", "removeLi(this)");
         list.appendChild(newLi);
     }
 }
+
 
 function pageList() {
 
     for (var keyStorage in localStorage) {
         var liItem = document.createElement("li");
         liItem.innerHTML = localStorage.getItem(keyStorage);
+        liItem.setAttribute("onclick", "removeLi(this)");
         document.getElementById("list").appendChild(liItem);
     }
 }
 pageList();
+
+
+function removeLi(myLi) {
+    myLi.parentNode.removeChild(myLi);
+    localStorage.removeItem(myLi.innerHTML);
+}
+
+
+
+
+
+
+
