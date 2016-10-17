114e0e85d17c009066ab3ba6aeb788278ca59660
Yurii Kotov <y_v_kotov@mail.ru>
2016-10-17 19:17:25 +0300

    done w4

--- app/pages/yurii-kotov/js/scripts.js
+++ app/pages/yurii-kotov/js/scripts.js
@@ -1,95 +1,99 @@
 /**
  * Created by Admin on 28.08.2016.
  */
-window.onload = function anyDoing() {
 
 
-    function addInfoAboutUser() {
-        var bornYear = ['16 ноября 1988г'];
-        var hobby = [' Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
-        hobby.push(' Хз что я еще могу делать');
 
-        var addYear = document.getElementById('user-info__date');
-        addYear.innerHTML = bornYear;
+function addInfoAboutUser() {
+    var bornYear = ['16 ноября 1988г'];
+    var hobby = [' Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
+    hobby.push(' Хз что я еще могу делать');
 
-        var addHobby = document.getElementById('user-info__hobby');
+    var addYear = document.getElementById('user-info__date');
+    addYear.innerHTML = bornYear;
 
-        hobby.forEach(function (item) {
-            var creatTag = document.createElement('li');
-            creatTag.className = 'user-info__hobby';
-            var addToHtml = addHobby.appendChild(creatTag);
-            if (item == hobby.slice(-1)) {
-                addToHtml.innerHTML = item + '... ';
-            } else {
-                addToHtml.innerHTML = item + ',';
-            }
-        });
-    }
-    addInfoAboutUser();
+    var addHobby = document.getElementById('user-info__hobby');
 
-    function getNewsFromGoogle() {
+    hobby.forEach(function (item) {
+        var creatTag = document.createElement('li');
+        creatTag.className = 'user-info__hobby';
+        var addToHtml = addHobby.appendChild(creatTag);
+        if (item == hobby.slice(-1)) {
+            addToHtml.innerHTML = item + '... ';
+        } else {
+            addToHtml.innerHTML = item + ',';
+        }
+    });
+}
+addInfoAboutUser();
 
-        var requestNews = new XMLHttpRequest();
-        var url = "news.json";
-        requestNews.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var blogNews = JSON.parse(this.responseText);
-                showNews(blogNews);
-                console.log(blogNews);
-            }
-        };
+function getNewsFromGoogle() {
 
-        requestNews.open('GET', url, true);
-        requestNews.send();
+    var requestNews = new XMLHttpRequest();
+    var url = "news.json";
+    requestNews.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var blogNews = JSON.parse(this.responseText);
+            showNews(blogNews);
+            console.log(blogNews);
+        }
+    };
 
-        function showNews(blogNews) {
-            var getNews = document.getElementById('getNews');
+    requestNews.open('GET', url, true);
+    requestNews.send();
+
+    function showNews(blogNews) {
+        var getNews = document.getElementById('getNews');
+        var createArtircleBlock = document.createElement('article');
+        document.getElementById('headBlog').innerHTML = blogNews.responseData.query;
+        for (var k in blogNews.responseData.entries) {
             var createArtircleBlock = document.createElement('article');
-            document.getElementById('headBlog').innerHTML = blogNews.responseData.query;
-            for (var k in blogNews.responseData.entries) {
-                var createArtircleBlock = document.createElement('article');
-                var addArticleBlock = getNews.appendChild(createArtircleBlock);
-                createArtircleBlock.style.marginBottom = '20px';
-                createArtircleBlock.className = 'col-md-10 col-md-offset-1';
-                var elmentShow = '<img href="' + blogNews.responseData.entries[k].url + '">' +
-                    '<h5>' + blogNews.responseData.entries[k].title + '</h5>' +
-                    '<p>' + blogNews.responseData.entries[k].contentSnippet + '</p>' +
-                    '<a href="' + blogNews.responseData.entries[k].link + '">' + blogNews.responseData.entries[k].link + '</a>';
-                addArticleBlock.innerHTML = elmentShow;
-            };
+            var addArticleBlock = getNews.appendChild(createArtircleBlock);
+            createArtircleBlock.style.marginBottom = '20px';
+            createArtircleBlock.className = 'col-md-10 col-md-offset-1';
+            var elmentShow = '<img href="' + blogNews.responseData.entries[k].url + '">' +
+                '<h5>' + blogNews.responseData.entries[k].title + '</h5>' +
+                '<p>' + blogNews.responseData.entries[k].contentSnippet + '</p>' +
+                '<a href="' + blogNews.responseData.entries[k].link + '">' + blogNews.responseData.entries[k].link + '</a>';
+            addArticleBlock.innerHTML = elmentShow;
         };
+    };
+}
+getNewsFromGoogle();
+
+
+function getToDoList() {
+    function eventSetToDo() {
+        var addLiMarker = document.getElementById('toDoList');
+        var buttonEvent = document.getElementById('submitToDo');
+
+        buttonEvent.addEventListener('click', function () {
+            var toDoAreaText = document.getElementById('toDoAreaText').value;
+            var newLi = document.createElement('li');
+            newLi.textContent = toDoAreaText;
+            localStorage.setItem(toDoAreaText, toDoAreaText);
+            addLiMarker.appendChild(newLi);
+            document.getElementById('toDoAreaText').setAttribute('onclick', 'value=""');
+        })
     }
-    getNewsFromGoogle();
+    eventSetToDo();
 
-    function getToDoList() {
-        function addOnPageList() {
-            var addLiMarker = document.getElementById('toDoList');
-            var buttonEvent = document.getElementById('submitToDo');
-            buttonEvent.addEventListener('click', function () {
-                var toDoAreaText = document.getElementById('toDoAreaText').value;
-                var newLi = document.createElement('li');
-                newLi.textContent = toDoAreaText;
-                localStorage.setItem(toDoAreaText, toDoAreaText);
-                addLiMarker.appendChild(newLi);
-                document.getElementById('toDoAreaText').setAttribute('onclick', 'value=""');
-            })
+
+
+    function callToDo() {
+
+        for (key in localStorage) {
+            var liOnPage = document.getElementById('toDoList').appendChild(document.createElement('li'));
+            liOnPage.innerHTML = localStorage.getItem(key);
+            liOnPage.setAttribute('onclick', 'removeThisLi(this)');
         }
-        addOnPageList();
-
-        function saveInLocalStorage() {
-
-            for (key in localStorage) {
-                var liOnPage = document.getElementById('toDoList').appendChild(document.createElement('li'));
-                liOnPage.innerHTML = localStorage.getItem(key);
-                liOnPage.addEventListener('click', function (event) {
-                    liOnPage.parentElement.removeChild(this);
-                    localStorage.removeItem(key)
-                });
-
-            };
-        }
-        saveInLocalStorage();
     }
-    getToDoList();
+    callToDo();
 
+}
+getToDoList();
+
+function removeThisLi(key) {
+    key.parentNode.removeChild(key);
+    localStorage.removeItem(key.innerHTML);
 }
