c2a629a832bb5ac3eae065cc0f77ae830b7b0188
Yurii Kotov <y_v_kotov@mail.ru>
2016-10-22 19:17:00 +0300

    fix

--- app/pages/yurii-kotov/js/scripts.js
+++ app/pages/yurii-kotov/js/scripts.js
@@ -1,112 +1,99 @@
 /**
  * Created by Admin on 28.08.2016.
  */
-window.onload = function anyDoing() {
-
-
-    /*function addInfoAboutUser() {
-        var bornYear = ['16 ноября 1988г'];
-        var hobby = [' Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
-        hobby.push(' Хз что я еще могу делать');
-
-        var addYear = document.getElementById('user-info__date');
-        addYear.innerHTML = bornYear;
-
-        var addHobby = document.getElementById('user-info__hobby');
-
-        hobby.forEach(function (item) {
-            var creatTag = document.createElement('li');
-            creatTag.className = 'user-info__hobby';
-
-
-            var addToHtml = addHobby.appendChild(creatTag);
-            if (item == hobby.slice(-1)) {
-                addToHtml.innerHTML = item + '... ';
-            } else {
-                addToHtml.innerHTML = item + ',';
-            }
-
-        });
-    };*/
-    function addInfoAboutUser() {
-        var bornYear = ['16 ноября 1988г'];
-        var hobby = ['Лентяйничать', "Спать", "Есть", "Синхрофазатрон", 'Хз что я еще могу делать'];
-
-        var addYear = document.getElementById('user-info__date');
-        var addHobby = document.getElementById('user-info__hobby');
-
-
-        addYear.innerHTML = bornYear;
-
-        hobby.forEach(function (item) {
-            var createTag = document.createElement('li');
-            createTag.className = 'user-info__hobby';
-
-            if (item == hobby.slice(-1)) {
-                item += '...';
-            } else {
-                item += ', ';
-            }
-            createTag.innerHTML = item;
-            addHobby.appendChild(createTag);
-        });
-    }
-
-    addInfoAboutUser();
-
-    function getNewsFromGoogle() {
-
-        var requestNews = new XMLHttpRequest();
-        var url = "news.json";
 
 
 
-        requestNews.open('GET', url, true);
-        requestNews.send();
+function addInfoAboutUser() {
+    var bornYear = ['16 ноября 1988г'];
+    var hobby = [' Лентяйничать', " Спать", " Есть", " Синхрофазатрон"];
+    hobby.push(' Хз что я еще могу делать');
 
-        requestNews.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var blogNews = JSON.parse(this.responseText);
-                showNews(blogNews);
-                console.log(blogNews);
+    var addYear = document.getElementById('user-info__date');
+    addYear.innerHTML = bornYear;
 
-            };
+    var addHobby = document.getElementById('user-info__hobby');
 
-        };
+    hobby.forEach(function (item) {
+        var creatTag = document.createElement('li');
+        creatTag.className = 'user-info__hobby';
+        var addToHtml = addHobby.appendChild(creatTag);
+        if (item == hobby.slice(-1)) {
+            addToHtml.innerHTML = item + '... ';
+        } else {
+            addToHtml.innerHTML = item + ',';
+        }
+    });
+}
+addInfoAboutUser();
 
-        function showNews(blogNews) {
-            var getNews = document.getElementById('getNews');
+function getNewsFromGoogle() {
+
+    var requestNews = new XMLHttpRequest();
+    var url = "news.json";
+    requestNews.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var blogNews = JSON.parse(this.responseText);
+            showNews(blogNews);
+            console.log(blogNews);
+        }
+    };
+
+    requestNews.open('GET', url, true);
+    requestNews.send();
+
+    function showNews(blogNews) {
+        var getNews = document.getElementById('getNews');
+        var createArtircleBlock = document.createElement('article');
+        document.getElementById('headBlog').innerHTML = blogNews.responseData.query;
+        for (var k in blogNews.responseData.entries) {
             var createArtircleBlock = document.createElement('article');
             var addArticleBlock = getNews.appendChild(createArtircleBlock);
-
-            for (var i in blogNews.responseData) {
-                document.getElementById('headBlog').innerHTML = blogNews.responseData.query;
-            };
-            for (var k in blogNews.responseData.entries) {
-                console.log(k);
-                var createArtircleBlock = document.createElement('article');
-                var addArticleBlock = getNews.appendChild(createArtircleBlock);
-                createArtircleBlock.style.marginBottom = '20px';
-                // createArtircleBlock.style.border = '1px solid gold';
-                createArtircleBlock.style.fontSize = '12px';
-                createArtircleBlock.className = 'col-md-10 col-md-offset-1';
-                var elmentShow = '<img href="' + blogNews.responseData.entries[k].url + '">' +
-                    '<h5>' + blogNews.responseData.entries[k].title + '</h5>' +
-                    '<p>' + blogNews.responseData.entries[k].contentSnippet + '</p>' +
-                    '<a href="' + blogNews.responseData.entries[k].link + '">' + blogNews.responseData.entries[k].link + '</a>';
-                addArticleBlock.innerHTML = elmentShow;
-            };
+            createArtircleBlock.style.marginBottom = '20px';
+            createArtircleBlock.className = 'col-md-10 col-md-offset-1';
+            var elmentShow = '<img href="' + blogNews.responseData.entries[k].url + '">' +
+                '<h5>' + blogNews.responseData.entries[k].title + '</h5>' +
+                '<p>' + blogNews.responseData.entries[k].contentSnippet + '</p>' +
+                '<a href="' + blogNews.responseData.entries[k].link + '">' + blogNews.responseData.entries[k].link + '</a>';
+            addArticleBlock.innerHTML = elmentShow;
         };
     };
-    getNewsFromGoogle();
-
-    function getToDoList() {
-        var buttonSub = document.getElementById('submitToDo');
-        buttonSub.addEventListener('submit', function () {
-
-        });
+}
+getNewsFromGoogle();
 
 
-    };
-    getToDoList();
-};
+function getToDoList() {
+    function eventSetToDo() {
+        var addLiMarker = document.getElementById('toDoList');
+        var buttonEvent = document.getElementById('submitToDo');
+
+        buttonEvent.addEventListener('click', function () {
+            var toDoAreaText = document.getElementById('toDoAreaText').value;
+            var newLi = document.createElement('li');
+            newLi.textContent = toDoAreaText;
+            localStorage.setItem(toDoAreaText, toDoAreaText);
+            addLiMarker.appendChild(newLi);
+            document.getElementById('toDoAreaText').setAttribute('onclick', 'value=""');
+        })
+    }
+    eventSetToDo();
+
+
+
+    function callToDo() {
+
+        for (key in localStorage) {
+            var liOnPage = document.getElementById('toDoList').appendChild(document.createElement('li'));
+            liOnPage.innerHTML = localStorage.getItem(key);
+            liOnPage.setAttribute('onclick', 'removeThisLi(this)');
+        }
+    }
+    callToDo();
+
+}
+getToDoList();
+
+function removeThisLi(key) {
+    key.parentNode.removeChild(key);
+    localStorage.removeItem(key.innerHTML);
+}
