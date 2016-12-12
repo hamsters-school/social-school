2d42c93c631332fb757d2dfe18e3afe6dfc29ae7
HomaX1 <baitasova.anastasiia@gmail.com>
2016-12-12 23:55:14 +0200

    finished slideShow with jQuery

--- app/pages/nastya-baitasova/js/jQuerySlideShow.js
+++ app/pages/nastya-baitasova/js/jQuerySlideShow.js
@@ -1,63 +1,69 @@
 
 $.when(
     $.ajax('https://pixabay.com/api/?key=2980920-46f1aa264b036ffc6e45ebad0&orientation=vertical&q=yellow+flowers&min_height=500'),
-    $.ajax('https://pixabay.com/api/?key=2980920-46f1aa264b036ffc6e45ebad0&orientation=vertical&q=red+flowers&min_height=500')
-).then(function(result1, result2){
+   $.ajax('https://pixabay.com/api/?key=2980920-46f1aa264b036ffc6e45ebad0&orientation=vertical&q=red+flowers&min_height=500')
+).then((result1, result2) => {
+
+
+    const WIDTHONE = 400;
+
 
 
     let results = result1[0].hits.concat(result2[0].hits);
+    let widthImgs = results.length * WIDTHONE;
 
-    $(results).each(function(index, value) {
+    $(results).each((index, value) => {
 
         //console.log(index, value.webformatURL);
 
-        $('.slides__showImg').prepend("<img src='" + value.webformatURL + "'>");
+        $('.slides__showImg').prepend("<img src='" + value.webformatURL + "'>").css({
+            'width': widthImgs + "px"
+        });
+    });
+
+
+    let arrows = 0;
+
+    $('.slides-wrap__leftArrow').click(() => {
+        if(arrows < 0) {
+            $('.slides__showImg').animate({
+                'left': arrows += WIDTHONE
+            },1000);
+        }
+        else {
+            return false;
+        }
+    });
+
+    let minWidth = widthImgs - WIDTHONE;
+
+      $('.slides-wrap__rightArrow').click(() => {
+        if(arrows > -minWidth) {
+            $('.slides__showImg').animate({
+                'left': arrows -= WIDTHONE
+            },1000);
+        }
+        else {
+            return false;
+        }
+    });
 
 
 
+    $('.slides__autoplay p input').change(() => {
+        setInterval(() => {
+            if(arrows > -minWidth) {
+                $('.slides__showImg').animate({
+                    'left': arrows -= WIDTHONE
+                },1000);
+            }
+            else {
+                return false;
+            }
+        }, 3000);
     });
 
 });
 
 
 
-
-
-
-
-
-
-/*Promise.all([
-    httpGet('https://pixabay.com/api/?key=2980920-46f1aa264b036ffc6e45ebad0&orientation=vertical&q=yellow+flowers&min_height=500'),
-    httpGet('https://pixabay.com/api/?key=2980920-46f1aa264b036ffc6e45ebad0&orientation=vertical&q=red+flowers&min_height=500')
-]).then(results => {
-
-
-    let elementId = document.getElementById('images');
-
-    for (let i = 0; i <= results.length; i++) {
-
-
-        let intermediateResult = results[i].hits;
-
-        for (let j = 0; j <= intermediateResult.length; j++) {
-            let pictures = document.createElement('img');
-
-            //console.log(intermediateResult[j].webformatURL);
-
-            pictures.setAttribute('src', intermediateResult[j].webformatURL);
-            pictures.setAttribute("onclick", "blogSelectedImg(this)");
-            elementId.appendChild(pictures);
-        }
-
-    }
-});
-
-let blogSelectedImg = (myImg) => {
-
-    let selectedImg = document.getElementById('selectedImg');
-    selectedImg.innerHTML = '';
-
-    selectedImg.appendChild(myImg.cloneNode(true));
-
-};*/
